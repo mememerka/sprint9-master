328da27b5a1d11c63c33853c1a5486ebab738423
Oriol <oriol.mercader@gmail.com>
2022-02-02 09:17:24 +0100

    2/2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Box } from '@mui/system';
 import Searchbar from './components/Searchbar/Searchbar';
 import VideoDetail from './components/VideoDetail/VideoDetail';
@@ -6,13 +6,16 @@ import VideoList from './components/VideoList/VideoList';
 import youtube from './api/youtube';
 import { useState } from 'react';
 import Provider from './aplication/Provider';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import img from './components/img/videoDetailnull.png'; 
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import CommentSection from './components/CommentSection/CommentSection';
-import { Container } from '@mui/material';
+import { Container, Button } from '@mui/material';
+import PrivateRoute from './components/CommentSection/commentprivate';
+import './App.css'
+import NotLogged from './components/NotLogged/NotLogged';
 
 function App() {
   const [videos,setVideos] = useState([]);
+  const [button, setButton] = useState(true);
 
   const handleSubmit = async (term) => {
     console.log('app',term) 
@@ -23,55 +26,76 @@ function App() {
     });
     setVideos(response.data.items);
     console.log(videos);
-  }; 
+  };
+
+  const showCommentButton = () => {
+    setButton(!button);
+  };
+
+  const styledMainBox = {
+    mt:5,
+    ml: 5,
+    display: 'flex',
+    flexFlow: 'row',
+    flexWrap: 'wrap',
+  };
+
+  const styledDetailMainBox = {
+    display: 'flex',
+  };
+
+  const styledDetailBox = {
+    width: 700,
+    mt: 5,
+    ml: 5,
+    display: 'flex',
+  };
+
+  const styledCommentsBox = {
+    display: 'flex',
+    justifyContent: 'center',
+    mt: 5
+  }
 
   return (
     <BrowserRouter>
      <Provider>
        <Container>
-        <Box sx={{
-          mt:5,
-          ml: 5,
-          display: 'flex',
-          flexFlow: 'row',
-          flexWrap: 'wrap',
-        }}>
+        <Box sx={styledMainBox}>
           <Box sx={{
             width: 1200
           }}> 
             <Searchbar handleFormSubmit={handleSubmit}/>
           </Box>
-          <Box sx={{
-            display: 'flex',
-          }}>
-            <Box sx={{
-              width: 700,
-              mt: 5,
-              ml: 5,
-              display: 'flex',
-              justifyContent: 'center'
-            }}>
-                <Routes>
-                  <Route exact path='/' element={<div>
-                                                  <img src={img} alt=''/>
-                                                </div>}/>
-                  <Route path='/videodetail' element={<VideoDetail/>}/>
-                  <Route path='*' element={<div>404</div> }/>
-                </Routes>
+          <Box sx={styledDetailMainBox}>
+            <Box sx={styledDetailBox}>
+              <VideoDetail/>
             </Box>
-            <Box sx={{
-              mt: 5,
-              ml: 10,
-            }}>
+            <Box>
               <VideoList videos={videos}/>
             </Box>
           </Box>
-          <CommentSection/>
         </Box>
+        <Box sx={styledCommentsBox}>
+        <Routes>
+              <Route element={<PrivateRoute/>}>
+                <Route path='/comments' element={<CommentSection/>}/>
+              </Route>
+              <Route path='/notLogged' element={<NotLogged/>}/>
+            </Routes>
+            <Link to='/comments' style={{ textDecoration: 'none' }}>
+              {
+              button && 
+              <Button variant="contained" color="error" onClick={showCommentButton}>
+                Comments
+              </Button>
+              }
+            </Link>
+        </Box> 
       </Container>
     </Provider>
   </BrowserRouter>
   );
-}
+};
 
 export default App;
